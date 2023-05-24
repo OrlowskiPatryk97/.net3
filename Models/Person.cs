eb0e791311ec29f8ce4dd02c6df94e8be49a240b
Patryk Orłowski <antikiller247@gmail.com>
2023-05-24 12:28:20 +0200

    fixed modelstate

--- Models/Person.cs
+++ Models/Person.cs
@@ -6,13 +6,13 @@ namespace FizzBuzzWeb.Models
 {
     public class Person
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         [Required]
         [MaxLength(100)]
         [Column(TypeName = "varchar(100)")]
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }
         [Required]
         [MaxLength(100)]
-        public string LastName { get; set; }
+        public string? LastName { get; set; }
     }
 }
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,36 +4,50 @@ using Newtonsoft.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Reflection;
+using FizzBuzzWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using FizzBuzzWeb.Data;
 
 namespace FizzBuzzWeb.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        [BindProperty]
-        public FizzBuzzForm FizzBuzz { get; set; }
+        private readonly PeopleContext _context;
+        /*[BindProperty]
+        public FizzBuzzForm? FizzBuzz { get; set; }
         [BindProperty(SupportsGet =true)]
         public string? Name { get; set; }
         public List<string> lista = new List<string>();
-        public string? output { get; set; }
+        public string? output { get; set; }*/
+        [BindProperty]
+        public Person Person { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IList<Person>? People { get; set; }
+
+  
+        public IndexModel(ILogger<IndexModel> logger, PeopleContext context)
         {
             _logger = logger;
+            _context = context;
         }
-
         public void OnGet()
         {
-            /*if (string.IsNullOrWhiteSpace(Name))
-            {
-                Name = "User";
-            }*/
+            People = _context.Person.ToList();
 
         }
 
         public IActionResult OnPost()
         {
-            var data = HttpContext.Session.GetString("Lista");
+            People = _context.Person.ToList();
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _context.Person.Add(Person);
+            _context.SaveChanges();
+            return RedirectToPage("./Index");
+            /*var data = HttpContext.Session.GetString("Lista");
 
             if (data != null)
             {
@@ -60,8 +74,8 @@ namespace FizzBuzzWeb.Pages
             }
             return Page();
 
+            */
 
-           
         }
 
     }
